fd8e6d184cdaebebe08239d8b6be8056b61736e4
Pranav Shyam <pranavshyam13@gmail.com>
2016-11-06 15:44:30 +0530

    revert the limiting. search the space!!

--- hyp_param_searcher.py
+++ hyp_param_searcher.py
@@ -4,7 +4,7 @@ from numpy.random import choice, random
 import os
 
 EXPT_START_NUM = 0
-NUM_EXPTS = 50
+NUM_EXPTS = 100
 
 for expt_num in range(EXPT_START_NUM, EXPT_START_NUM + NUM_EXPTS):
 	arg_string = 'python arc_omniglot_verif.py'
@@ -12,8 +12,8 @@ for expt_num in range(EXPT_START_NUM, EXPT_START_NUM + NUM_EXPTS):
 	arg_string += " -n " + str(expt_num) 							# expt-name
 	arg_string += " -l " + str(10.0 ** (-4 * random() - 2)) 		# learning-rate
 	arg_string += " -i " + str(32)									# image-size
-	arg_string += " -a " + str(choice([4, 6])) 						# attn-win
-	arg_string += " -s " + str(choice([256, 512, 1024]))			# lstm-states
+	arg_string += " -a " + str(choice([4, 6, 8])) 					# attn-win
+	arg_string += " -s " + str(choice([256, 512, 1024, 2048]))		# lstm-states
 	arg_string += " -g " + str(choice([8, 16])) 					# glimpses
 	arg_string += " -f " + str(choice([0.25, 0.5, 1.0, 2.0])) 		# fg-bias
 	arg_string += " -b " + str(choice([32, 64])) 					# batch-size
