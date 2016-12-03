f666a01c6cc6795a86198b53ffeb586fb4cfb9d4
Pranav Shyam <pranavshyam13@gmail.com>
2016-12-03 15:12:35 +0530

    fix BDRNN, use ADAM

--- full_conext_os.py
+++ full_conext_os.py
@@ -10,7 +10,7 @@ from lasagne.nonlinearities import rectify, softmax
 from lasagne.init import HeNormal
 from lasagne.layers import get_all_params, get_output
 from lasagne.objectives import categorical_crossentropy, categorical_accuracy
-from lasagne.updates import rmsprop
+from lasagne.updates import adam
 from lasagne.layers import helper
 
 from main import train, test, serialize, deserialize
@@ -21,12 +21,12 @@ print "... setting up the network"
 X = T.tensor3("input")
 y = T.ivector("target")
 
+num_states = 64
 l_in = InputLayer(shape=(None, 20, 512), input_var=X)
-l_lstm1 = LSTMLayer(l_in, 64)
-l_lstm2 = LSTMLayer(l_in, 64)
-l_rev2 = ExpressionLayer(l_lstm2, lambda I: I[:,range(19, -1, -1)], output_shape='auto')
-l_merge = ConcatLayer([l_lstm1, l_rev2], axis=2)
-l_rshp1 = ReshapeLayer(l_merge, (-1, 128))
+l_lstm_up = LSTMLayer(l_in, num_states)
+l_lstm_down = LSTMLayer(l_in, num_states, backwards=True)
+l_merge = ElemwiseSumLayer([l_lstm_up, l_lstm_down])
+l_rshp1 = ReshapeLayer(l_merge, (-1, num_states))
 l_dense = DenseLayer(l_rshp1, 1, W=HeNormal(gain='relu'), nonlinearity=rectify)
 l_rshp2 = ReshapeLayer(l_dense, (-1, 20))
 l_y = NonlinearityLayer(l_rshp2, softmax)
@@ -37,14 +37,14 @@ loss = T.mean(categorical_crossentropy(prediction, y))
 accuracy = T.mean(categorical_accuracy(prediction, y))
 
 params = get_all_params(l_y, trainable=True)
-updates = rmsprop(loss, params, learning_rate=1e-2)
+updates = adam(loss, params, learning_rate=1e-3)
 
 print "... compiling"
 train_fn = theano.function(inputs=[X, y], outputs=loss, updates=updates)
 val_fn = theano.function(inputs=[X, y], outputs=[loss, accuracy])
 
 print "... loading data"
-worker = OmniglotOS('ARC_VERIF_omniglot_standard', 512, within_alphabet=True, num_trails=32)
+worker = OmniglotOS('ARC_VERIF_omniglot_standard', 512, within_alphabet=True, num_trails=64)
 
 meta_data = {}
 meta_data["n_iter"] = 100000
